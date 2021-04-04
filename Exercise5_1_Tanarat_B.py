d5f1fbdb1432328232d836c8849276e0ef9eea5a
tanaratbanthip <81560207+tanaratbanthip@users.noreply.github.com>
2021-04-04 17:07:41 +0700

    Add files via upload

---
+++ Exercise5_1_Tanarat_B.py
@@ -0,0 +1,7 @@
+print("โปรแกรมคำนวณ บวก ลบ คูณ หาร ")
+Number1 = int(input("โปรดระบุตัวเลข : "))
+Number2 = int(input("โปรดระบุตัวเลข : "))
+print(Number1,"+",Number2,"=",Number1+Number2)
+print(Number1,"-",Number2,"=",Number1-Number2)
+print(Number1,"x",Number2,"=",Number1*Number2)
+print(Number1,"/",Number2,"=",int(Number1/Number2))
