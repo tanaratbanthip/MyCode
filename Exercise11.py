5ce12355fc83df1e6b144f71bec72cb9618392cb
tanaratbanthip <81560207+tanaratbanthip@users.noreply.github.com>
2021-04-13 19:51:17 +0700

    Add files via upload

---
+++ Exercise11.py
@@ -0,0 +1,12 @@
+print("input","Output")
+print("5")
+null = 9
+print(" "*null+"*")
+null = 9 - 1
+print(" "*null+"***")
+null = 9 - 2
+print(" "*null+"*****")
+null = 9 - 3
+print(" "*null+"*******")
+null = 9 - 4
+print(" "*null+"*********")
