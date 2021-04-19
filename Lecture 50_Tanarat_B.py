19e9c473b95d040b3ae8faefbf8b80149c141228
tanaratbanthip <81560207+tanaratbanthip@users.noreply.github.com>
2021-04-19 10:04:57 +0700

    Add files via upload

---
+++ Lecture 50_Tanarat_B.py
@@ -0,0 +1,15 @@
+def addNumber(a,b):
+    print(a+b)
+addNumber(20,10)
+
+def minusNumber(a,b):
+    print(a-b)
+minusNumber(20,10)
+
+def timesNumber(a,b):
+    print(a*b)
+timesNumber(20,10)
+
+def dividedNumber(a,b):
+    print(a/b)
+dividedNumber(20,10)
